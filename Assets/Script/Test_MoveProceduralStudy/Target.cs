4ac996d675d6b9d25f61b34b7b4354fff3da8b7e
DarkBenve <cosimo.benvenuti10@gmail.com>
2024-06-10 20:07:29 +0200

    Add_Test_Humanoid

---
+++ Assets/Script/Test_MoveProceduralStudy/Target.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using cmdwtf.UnityTools.Dynamics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Script.Test_MoveProceduralStudy
+{
+    public class Target : MonoBehaviour
+    {
+        [SerializeField] private List<DynamicsTransform> dynamicsTransform;
+
+        private void Start()
+        {
+            foreach (var dynamics in dynamicsTransform)
+            {
+                dynamics.SetTarget(gameObject.transform);
+            }
+        }
+    }
+}
